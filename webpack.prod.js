9b56f296885f6a21bd18e8fc3209ecf886416475
Edvinas Aukstakalnis <ed@Edvinass-MacBook-Air.local>
2021-05-15 19:24:26 +0100

    set up html and file loader loader to access files e.g.png from anywhere and set up clean webpack to remove old build files

--- webpack.prod.js
+++ webpack.prod.js
@@ -1,9 +1,11 @@
 const path = require("path");
 const common = require("./webpack.common");
 const merge = require("webpack-merge");
+const { CleanWebpackPlugin } = require("clean-webpack-plugin");
 
 module.exports = merge(common, {
 	mode: "production",
+	plugins: [new CleanWebpackPlugin()],
 	devtool: "eval-source-map",
 	resolve: {
 		extensions: [".ts", ".js"],
